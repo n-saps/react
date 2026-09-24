95ada04d2f1edaed383279ac10e8cd80585e8956
n-saps <neetu15032004@gmail.com>
2026-09-24 12:03:37 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Card from "./components/Card.jsx";
+import { useState } from "react";
 
-const App = () =>{
-  return <>
-  <div className="cards">
-    <Card/>
-    <Card/>
-  </div>
-  </>
-}
+const App = () => {
+  const [name, setName] = useState("");
+  const onFormSubmit = (e) =>{
+    e.preventDefault();
+    console.log(name);
+
+    setName('');
+  }
+
+  return (
+    <>
+      <form onSubmit={(e) => {onFormSubmit(e)}}>
+        <input
+          className="bg-amber-700 p-1 font-bold text-2xl m-1"
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <button className="bg-amber-300 p-1 text-2xl font-bold m-1">Submit</button>
+      </form>
+    </>
+  );
+};
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
-const Card = () => {
+const Card = ({ title, description, image }) => {
   return (
-    <>
-      <div>
-        <h1 className="">This is a card</h1>
+    <div className="max-w-sm overflow-hidden rounded-xl bg-neutral-200 shadow-md">
+      <img
+        src={image}
+        alt={title}
+        className="h-48 w-full object-cover"
+      />
+
+      <div className="p-5">
+        <h2 className="mb-2 text-xl font-bold text-gray-800">
+          {title}
+        </h2>
+
+        <p className="mb-4 text-gray-600">
+          {description}
+        </p>
+
+        <button className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
+          View Details
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
